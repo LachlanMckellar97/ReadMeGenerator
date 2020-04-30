bb2740acc9538c5388e7df23b8378ba59818c42f
Lachlan Mckellar <lachlan.person@gmail.com>
2020-04-30 22:14:17 +1000

    removing my apikey

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
 
-const apiKey = "4944f48d1540deb12af5a52f2d194e082808b281 "; // insert your GitHub API key inside the quotation marks
+const apiKey = ""; // insert your GitHub API key inside the quotation marks
 
 const markdown = require("./utils/generateMarkdown.js")
 
